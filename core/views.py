d8c31a70707dd8ac750085ad9beebbf08532b3a8
Joseph Ampadu <jkampadujnr@gmail.com>
2023-06-12 11:57:41 +0000

    Add account verification

--- core/views.py
+++ core/views.py
@@ -1,7 +1,14 @@
-from django.urls import reverse_lazy
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.utils.encoding import force_str
+from django.utils.http import urlsafe_base64_decode
 from django.views import generic
 
 from .forms import SignUpForm
+from .mail import send_account_verification_mail
+from .models import User
+from .tokens import account_activation_token
 
 
 class SignUpView(generic.CreateView):
@@ -9,7 +16,43 @@ class SignUpView(generic.CreateView):
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["password1_help_text"] = context["form"].fields["password1"].help_text
-        return context
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(
+            request,
+            self.template_name,
+            {
+                "form": form,
+                "password1_help_text": form.fields["password1"].help_text,
+            },
+        )
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.save()
+            email = form.cleaned_data.get("email")
+            send_account_verification_mail(request, user, email)
+            return HttpResponseRedirect(reverse("login"))
+        return render(
+            request,
+            self.template_name,
+            {
+                "form": form,
+                "password1_help_text": form.fields["password1"].help_text,
+            },
+        )
+
+
+def activate_account(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        return HttpResponseRedirect(reverse("login"))
+    return HttpResponse("Activation link is invalid")
